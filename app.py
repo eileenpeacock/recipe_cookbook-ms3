f48241733afb9104ccd8f476d96bed8e65aa97d7
Eileen Peacock <ejpeacock@hotmail.com>
2019-10-22 22:20:08 +0100

    check global settings

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ def hello():
     return 'Hello World ...testing'
 
 
-# Global settings
-# if __name__ == '__main__':      
-#     app.run(host=os.environ.get('IP'),              
-#     port=int(os.environ.get('PORT')),              
-#     debug=True)
+Global settings
+if __name__ == '__main__':      
+    app.run(host=os.environ.get('IP'),              
+    port=int(os.environ.get('PORT')),              
+    debug=True)
 
-# local settings
-if __name__=='__main__':
-    app.run(host = '0.0.0.0',
-    port = int('8080'),
-    debug=True) 
+# # local settings
+# if __name__=='__main__':
+#     app.run(host = '0.0.0.0',
+#     port = int('8080'),
+#     debug=True) 
