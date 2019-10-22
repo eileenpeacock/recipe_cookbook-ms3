922a7cd7da92919b8e6e3f280d21e3b8626ba199
Eileen admin <eileenadmin@Eileens-MBP.connect>
2019-10-22 21:38:03 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+#import modules
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello World ...testing'
+
+
+# Global settings
+# if __name__ == '__main__':      
+#     app.run(host=os.environ.get('IP'),              
+#     port=int(os.environ.get('PORT')),              
+#     debug=True)
+
+# local settings
+if __name__=='__main__':
+    app.run(host = '0.0.0.0',
+    port = int('8080'),
+    debug=True) 
